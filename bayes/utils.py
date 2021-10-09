cd2164481c70e17ef9b3a905c399bc90dea9e055
aianman4823 <akitoaianman1@gmail.com>
2021-10-09 17:43:04 +0900

    多次元正規分布に対する便利関数追加

--- bayes/utils.py
+++ bayes/utils.py
@@ -61,3 +61,63 @@ def make_xline(mu, lmb, n):
                         mu + 4 * np.sqrt(1 / lmb),
                         num=n)
     return xline
+
+
+def plot_likelihood_2d(x_0_grid,
+                       x_1_grid,
+                       x_dims,
+                       model,
+                       xlabel='x',
+                       ylabel='density',
+                       color='purple',
+                       suptitle='default_suptitle',
+                       title='default_title',
+                       output_path='../outputs/default',
+                       name='default',
+                       truth=None,
+                       true_model=None,
+                       x_nd=None):
+    fig = plt.figure(figsize=(12, 9))
+    plt.contour(x_0_grid, x_1_grid, model.reshape(x_dims))  # 尤度
+    if truth is not None:
+        plt.scatter(x=truth[0], y=truth[1], color='red', s=100, marker='x')
+    if x_nd is not None:
+        plt.scatter(x=x_nd[:, 0], y=x_nd[:, 1])  # 観測データ
+
+    if true_model is not None:
+        plt.contour(x_0_grid, x_1_grid, true_model.reshape(x_dims), alpha=0.5, linestyles='--')  # 尤度
+
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.suptitle(suptitle, fontsize=20)
+    plt.title(title, loc='left')
+    plt.colorbar()
+    my_makedirs(output_path)
+    fig.savefig(output_path + '/' + name)
+    pass
+
+
+def plot_scatter_2d(x_nd,
+                    x_0_grid,
+                    x_1_grid,
+                    x_dims,
+                    model,
+                    xlabel='x',
+                    ylabel='density',
+                    color='purple',
+                    suptitle='default_suptitle',
+                    title='default_title',
+                    output_path='../outputs/default',
+                    name='default.png',
+                    truth=None,
+                    true_model=None):
+    plt.figure(figsize=(12, 9))
+    plt.scatter(x=x_nd[:, 0], y=x_nd[:, 1])  # 観測データ
+    plt.contour(x_0_grid, x_1_grid, model.reshape(x_dims))  # 真の分布
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.suptitle(suptitle, fontsize=20)
+    plt.title(title, loc='left')
+    plt.colorbar()
+    my_makedirs(output_path)
+    plt.savefig(output_path + name)
